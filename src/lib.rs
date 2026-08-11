1b8809e7a08004a579c1e34a135b8f4381c2ca6a
Romet Tagobert <romet@tagobert.me>
2026-08-11 15:19:24 +0300

    parse the footer

--- src/lib.rs
+++ src/lib.rs
@@ -24,10 +24,73 @@ pub struct Nif {
     ))]
     #[br(parse_with = parse_utils::parse_blocks)]
     pub blocks: Vec<blocks::Block>,
+    pub footer: Footer,
+}
+
+#[derive(Debug, PartialEq, BinRead)]
+pub struct Footer {
+    pub num_roots: u32,
+    #[br(count = num_roots)]
+    pub root_refs: Vec<common::BlockRef>,
 }
 
 impl Nif {
     pub fn parse<R: Read + Seek>(reader: &mut R) -> Result<Self, NifError> {
         Ok(reader.read_le()?)
+    }
+
+    pub fn roots(&self) -> impl Iterator<Item = (usize, &blocks::Block)> {
+        self.footer.root_refs.iter().filter_map(|r| {
+            usize::try_from(r.0)
+                .ok()
+                .and_then(|i| self.blocks.get(i).map(|b| (i, b)))
+        })
+    }
+
+    pub fn walk(&self) -> walk::Walk<'_> {
+        walk::Walk::from_roots(&self.blocks, self.roots().map(|(i, _)| i))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Cursor;
+
+    fn load(n: u32) -> Nif {
+        let bytes = std::fs::read(format!("tests/{}.nif", n)).expect("read nif");
+        Nif::parse(&mut Cursor::new(bytes)).expect("parse nif")
+    }
+
+    #[test]
+    fn every_fixture_has_resolvable_roots() {
+        for n in 1..=26 {
+            let nif = load(n);
+            assert!(!nif.footer.root_refs.is_empty(), "file {} has no roots", n);
+            assert_eq!(
+                nif.roots().count(),
+                nif.footer.root_refs.len(),
+                "file {} has a root ref pointing outside blocks",
+                n
+            );
+        }
+    }
+
+    #[test]
+    fn walk_starts_from_the_declared_roots() {
+        for n in 1..=26 {
+            let nif = load(n);
+            let first = nif.walk().next().expect("at least one visit");
+            let (root_index, _) = nif.roots().next().expect("at least one root");
+            assert_eq!(first.index, root_index, "file {}", n);
+            assert_eq!(first.depth, 0);
+        }
+    }
+
+    #[test]
+    fn roots_skip_negative_refs() {
+        let mut nif = load(1);
+        nif.footer.root_refs.push(common::BlockRef(-1));
+        assert_eq!(nif.roots().count(), nif.footer.root_refs.len() - 1);
     }
 }
